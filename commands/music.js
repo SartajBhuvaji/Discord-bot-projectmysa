cab21d46ee99b75e70750ee1c84368fd3643c31a
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2021-05-12 18:00:52 +0530

    Update music.js

--- commands/music.js
+++ commands/music.js
@@ -9,10 +9,32 @@ const { prefix, token } = require("../config");
 const ytdl = require("ytdl-core");
 const client = new Discord.Client();
 const queue = new Map();
-module.exports = (bot,message)=>{ 
+
+module.exports = (bot,message)=>{
+  //---------------
+  /*
+  bot.on('voiceStateUpdate', (oldState, newState) => {
+
+    // if nobody left the channel in question, return.
+    if (oldState.channelID !==  oldState.guild.me.voice.channelID || newState.channel)
+      return;
+  
+    // otherwise, check how many people are in the channel now
+    if (!oldState.channel.members.size - 1) 
+      setTimeout(() => { // if 1 (you), wait five minutes
+        if (!oldState.channel.members.size - 1) // if there's still 1 member, 
+           oldState.channel.leave(); // leave
+       }, 300000); // (5 min in ms)
+  });
+  */
+  
+
+  //---------------------------
     try{
   if (message.author.bot) return;
   const serverQueue = queue.get(message.guild.id);
+
+  //serverQueue.connection.dispatcher.end();
 
   if (message.content.substring().split(" ")[0] === "!sa" && message.content.substring().split(" ")[1]=== "play") {
     execute(message, serverQueue);
@@ -107,6 +129,7 @@ function stop(message, serverQueue) {
     
   serverQueue.songs = [];
   serverQueue.connection.dispatcher.end();
+  return message.channel.send("Stopped !");
 }
 
 function play(guild, song) {
@@ -125,7 +148,7 @@ function play(guild, song) {
     })
     .on("error", error => console.error(error));
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
+  serverQueue.textChannel.send(`Now playing: **${song.title}**`);
 
   
 }
